82e41ca19889e1430fe6810744e9f0724da84609
chaos29092 <wo94yun>
2014-12-23 14:47:05 +0800

    Initial commit

---
+++ app/config/packages/bogardo/mailgun/config.php
@@ -0,0 +1,81 @@
+<?php
+
+return array(
+
+	/**
+	 * You may wish for all e-mails sent with Mailgun to be sent from
+	 * the same address. Here, you may specify a name and address that is
+	 * used globally for all e-mails that are sent by Mailgun.
+	 *
+	 */
+	'from' => array(
+		'address' => 'info@email.hanvymbh.com',
+		'name' => 'chaos'
+	),
+
+
+	/**
+	 * Global reply-to e-mail address
+	 *
+	 */
+	'reply_to' => 'info@email.hanvymbh.com',
+
+
+	/**
+	 * Mailgun (private) API key
+	 *
+	 */
+	'api_key' => 'key-e7a2189e5b3aaa5466ec0f30badfb906',
+
+	/**
+	 * Mailgun public API key
+	 *
+	 */
+	'public_api_key' => 'pubkey-8b2fa675b45fd7dc42df8a13ad216605',
+
+	/**
+	 * Domain name registered with Mailgun
+	 *
+	 */
+	'domain' => 'email.hanvymbh.com',
+
+	/**
+	 * Force the from address
+	 *
+	 * When your `from` e-mail address is not from the domain specified some
+	 * e-mail clients (Outlook) tend to display the from address incorrectly
+	 * By enabling this setting Mailgun will force the `from` address so the
+	 * from address will be displayed correctly in all e-mail clients.
+	 *
+	 * Warning:
+	 * This parameter is not documented in the Mailgun documentation
+	 * because if enabled, Mailgun is not able to handle soft bounces
+	 *
+	 */
+	'force_from_address' => false,
+
+
+	/**
+	 * Testing
+	 *
+	 * Catch All address
+	 *
+	 * Specify an email address that receives all emails send with Mailgun
+	 * This email address will overwrite all email addresses within messages
+	 */
+	'catch_all' => "178399731@qq.com",
+
+
+	/**
+	 * Testing
+	 *
+	 * Mailgun's testmode
+	 *
+	 * Send messages in test mode by setting this setting to true.
+	 * When you do this, Mailgun will accept the message but will
+	 * not send it. This is useful for testing purposes.
+	 *
+	 * Note: Mailgun does charge your account for messages sent in test mode.
+	 */
+	'testmode' => true
+);
