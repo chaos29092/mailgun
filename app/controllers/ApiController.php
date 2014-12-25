eb554ba4cb92ce782a685f86c3ea2cd379416e0e
chaos29092 <wo94yun>
2014-12-25 17:31:14 +0800

    small fix

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -20,7 +20,7 @@ class ApiController extends BaseController {
         $mgClient = new Mailgun('key-42b2ca9deaeaa59566bc33a1c5462210');
         $domain = 'email.hanvymbh.com';
 
-        $result = $mgClient->get("$domain/campaigns", array('limit' => 2));
+        $result = $mgClient->get("$domain/campaigns/test/events", array('limit' => 100));
 
         $data['result'] = object_array($result);
         return View::make('test',$data);
