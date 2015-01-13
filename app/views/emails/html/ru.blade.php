bb5d540cea78f7bdb8eb6e10466036842362e588
chaos29092 <wo94yun>
2015-01-13 14:18:53 +0800

    ru mail

--- app/views/emails/html/ru.blade.php
+++ app/views/emails/html/ru.blade.php
@@ -138,19 +138,6 @@
                         </td>
                     </tr>
                 </table>
-                <div class="footer"
-                     style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
-                    <table width="100%"
-                           style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
-                        <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
-                            <td class="aligncenter content-block"
-                                style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;"
-                                align="center" valign="top"><a href="%unsubscribe_url%"
-                                                                      style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0; padding: 0;">unsubscribe</a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
             </div>
         </td>
         <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;"
